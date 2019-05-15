11eec7125e9d563ab5547742361c18caa80ef2e5
Chris Whited <chris.whited@springventuregroup.com>
2019-05-14 22:36:56 -0500

    fix formatting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,7 +11,15 @@ export const environment: EnvModel = {
     successUrl: '/github-user-search',
     responseType: 'token id_token',
     mgmtToken:
-      'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlF6RTJPRFpGUkRoR1FUa3pNa00yTWpFek5qWkdOVVZCT1RRMU5UTXhOa1pCTWpReE5qVTRSUSJ9.eyJpc3MiOiJodHRwczovL2Nocmlzd2hpdGVkLmF1dGgwLmNvbS8iLCJzdWIiOiJyaVdQMVROR1BKMVRldzFqdXNPcVhSbkpkUkdycUpES0BjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9jaHJpc3doaXRlZC5hdXRoMC5jb20vYXBpL3YyLyIsImlhdCI6MTU1Nzg5MDMwNCwiZXhwIjoxNTU3OTc2NzA0LCJhenAiOiJyaVdQMVROR1BKMVRldzFqdXNPcVhSbkpkUkdycUpESyIsInNjb3BlIjoicmVhZDp1c2VycyByZWFkOnVzZXJfaWRwX3Rva2VucyIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.SI600O1-_d86-P3w7s9g4MggD7T1cAc-YD8XGLt7LTQoduR4BqHfQQN0cLxqmLDCKVyKwQvX2wcc9NMQPzi6NAHtejxnAYNNfRdWiFNRNoZjlbiZ_hK64SdXN5z7WAtrHRx4UwMby7G2dQMh3p7a7TVUrCDT31xgcLuJj7v6LtniHsSkWBOUb6avBN5gJLRNufGlFtA_MySviYtceaT71cpoZbHkcS5kMMmaVQrd2b1h0TPRN4EJ0X0UH_OfCQXPmsrQN0ue2s00y11zf6bwVQD6TaImgLuoaEvugbk8uMRF_vYUKpB8Akn8SnOMweDzKWWGJVlLr_eYNcuIZbjBzg'
+      'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlF6RTJPRFpGUkRoR1FUa3pNa00yTWpFek5qWkdOVVZCT1RRMU5UTXh' +
+      'Oa1pCTWpReE5qVTRSUSJ9.eyJpc3MiOiJodHRwczovL2Nocmlzd2hpdGVkLmF1dGgwLmNvbS8iLCJzdWIiOiJyaVdQMVROR1BK' +
+      'MVRldzFqdXNPcVhSbkpkUkdycUpES0BjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9jaHJpc3doaXRlZC5hdXRoMC5jb20vYXBpL3Y' +
+      'yLyIsImlhdCI6MTU1Nzg5MDMwNCwiZXhwIjoxNTU3OTc2NzA0LCJhenAiOiJyaVdQMVROR1BKMVRldzFqdXNPcVhSbkpkUkdycU' +
+      'pESyIsInNjb3BlIjoicmVhZDp1c2VycyByZWFkOnVzZXJfaWRwX3Rva2VucyIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9' +
+      '.SI600O1-_d86-P3w7s9g4MggD7T1cAc-YD8XGLt7LTQoduR4BqHfQQN0cLxqmLDCKVyKwQvX2wcc9NMQPzi6NAHtejxnAYNNfR' +
+      'dWiFNRNoZjlbiZ_hK64SdXN5z7WAtrHRx4UwMby7G2dQMh3p7a7TVUrCDT31xgcLuJj7v6LtniHsSkWBOUb6avBN5gJLRNufGlFt' +
+      'A_MySviYtceaT71cpoZbHkcS5kMMmaVQrd2b1h0TPRN4EJ0X0UH_OfCQXPmsrQN0ue2s00y11zf6bwVQD6TaImgLuoaEvugbk8uM' +
+      'RF_vYUKpB8Akn8SnOMweDzKWWGJVlLr_eYNcuIZbjBzg'
   },
   githubGraphQLUrl: 'https://api.github.com/graphql'
 };
